5f070207cec74d1d75f698392b9a8867b21236c1
Karoline Nylaender <karolmny@stud.ntnu.no>
2020-07-22 11:44:29 +0200

    la til snake

--- game/views.py
+++ game/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "game/index.html")
+
+def snake(request):
+    return render(request, "game/snake/snake.html")
